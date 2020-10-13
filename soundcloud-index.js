be47af40ac24ad0e763ad3a5b54355438c9cae4e
amalatawy <amalatawy@gmail.com>
2020-10-13 18:08:14 +0200

    increase limit to 2 and half hours

--- soundcloud-index.js
+++ soundcloud-index.js
@@ -215,7 +215,7 @@ nCallBack.onInlineMessageCallback = async (inlineMsgCallback) => {
         scdl
           .getInfo(btnCallback, CLIENT_ID)
           .then(({ full_duration, title }) => {
-            if (full_duration > 5400000) {
+            if (full_duration > 9000000) {
               console.log(full_duration);
               api.sendText(
                 inlineMsgCallback.chat.id,
